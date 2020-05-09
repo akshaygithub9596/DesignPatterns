f22c224400193f5676256950d0c60dcb8b997e06
akshay <akshaygadavey@gmail.com>
2020-05-09 06:33:32 +0530

    enum

---
+++ SingletonJAVAClasses/7Proj-Singleton_ENUM/src/com/exps/sdp/Printer.java
@@ -0,0 +1,9 @@
+package com.exps.sdp;
+
+public enum Printer {
+	INSTANCE;
+
+	public void getData(String data) {
+		System.out.println("Printer.getData()");
+	}
+}
---
+++ SingletonJAVAClasses/7Proj-Singleton_ENUM/src/com/exps/test/EnumTest.java
@@ -0,0 +1,16 @@
+package com.exps.test;
+
+import com.exps.sdp.Printer;
+
+public class EnumTest {
+
+	public static void main(String[] args) {
+		Printer p1 = null, p2 = null;
+		p1 = Printer.INSTANCE;
+		p2 = Printer.INSTANCE;
+		System.out.println(p1.hashCode() + " === " + p2.hashCode());
+		System.out.println("(p1==p2 ?)" + (p1 == p2));
+
+	}
+
+}
